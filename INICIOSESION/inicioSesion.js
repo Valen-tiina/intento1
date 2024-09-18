7e399367dabb4efea973e91ebdb730ae7363d311
Valentina Rodriguez <vale.rodriguez.palacios@gmail.com>
2024-09-17 22:04:00 -0500

    final

--- INICIOSESION/inicioSesion.js
+++ INICIOSESION/inicioSesion.js
@@ -1,24 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let Usuarios = [
-        { email: "laura@gmail.com", password: "111" },
-        { email: "juan@gmail.com", password: "222" }
-    ];
+    const form = document.getElementById('loginForm');
 
-    const Form = document.getElementById('loginForm');
-
-    Form.addEventListener("submit", e => {
-        e.preventDefault(); //evitar que se sobrecargue la pagina
+    form.addEventListener("submit", e => {
+        e.preventDefault(); 
 
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
-        const userExists = Usuarios.find(user => user.email === email);
-        if (userExists && userExists.password === password) {
-            alert("cuenta encontrada");
-            window.location.href = '/COMPRA/w1.html'//para que el boton me diriga al vinculo deseado si se cumple la funcion
+        // Obtener usuarios del localStorage
+        let usuarios = JSON.parse(localStorage.getItem('Usuarios')) || [];
 
-} else {
-    alert("cuenta no encontrada vuelve a intertarlo de nuevo");
-}
+        // Verificar si el usuario existe y la contraseña es correcta
+        const userExists = usuarios.find(user => user.email === email);
+        if (userExists && userExists.password === password) {
+            alert("Cuenta encontrada");
+            window.location.href = '/COMPRA/w1.html'; 
+        } else {
+            alert("Los Datos encontrados no son correctos");
+        }
     });
 });
